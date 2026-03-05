7de01c098c8f4d4da55de6eb7b964f229e41120e
Ukasha Anwar <ukashaanwar774@gmail.com>
2026-03-05 23:29:31 +0500

    Minor Commit

--- src/defines.h
+++ src/defines.h
@@ -18,9 +18,9 @@ constexpr int JUMP_FORCE = 700;
 enum GameStates
 {
     Menu = 0,
-    Starting = 1,
-    Playing = 2,
-    GameOver = 3
+    Starting,
+    Playing,
+    GameOver
 };
 
 enum ObjectTypes
